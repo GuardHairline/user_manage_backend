c2652c2cfd641c65f34b1e5ae36c6d3e54a1eedb
lx <445462184@qq.com>
2024-08-23 09:58:02 +0800

    初始化

---
+++ src/main/java/com/example/umb/UserManageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.umb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManageBackendApplication.class, args);
+    }
+
+}
