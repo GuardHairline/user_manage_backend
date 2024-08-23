ab7ca795ec7fa4375ef41102e67c05d84efb4d25
lx <445462184@qq.com>
2024-08-23 10:45:09 +0800

    配置Mybatis数据源

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,7 +4,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import static org.springframework.boot.SpringApplication.run;
-
+@SpringBootApplication
 public class Application {
     public static void main(String[] args){
         ConfigurableApplicationContext run = run(Application.class, args);
